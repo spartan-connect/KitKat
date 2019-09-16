34e1836b51bfc2703b35a47e1b7766f4c0d28ae4
Maan Singh <maan.vs97@gmail.com>
2019-09-16 16:00:47 -0700

    Added some basic functionality like login and profile page

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+// app.get('/profile/:name', function(req, res){
+//   var data = {age: 22, job: 'Student'};
+//   res.render('profile', {person: req.params.name, data: data});
+// });
+
+app.post('/profile', urlencodedParser, function(req, res){
+  console.log(req.body);
+  res.render('profile', {data: req.body});
+});
+
+app.listen(3000);
