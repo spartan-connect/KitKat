e7d7c356686537bf1797f3ac2fd3c0fe818db605
Dylan Ceronio <dylanceronio@sbcglobal.net>
2019-09-20 12:47:03 -0700

    The files will now save the info entered in the login to the database on mongo atlas

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var dbModels = require('./dbfiles/schema.js');
+
+//for db
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.render('home');
 });
 
@@ -16,13 +21,23 @@ app.get('/', function(req, res){
 //   res.render('profile', {person: req.params.name, data: data});
 // });
 
-app.post('/profile', urlencodedParser, function(req, res){
+// 400 status is an operation failed
+app.post('/profile', function (req, res) {
   console.log(req.body);
-  res.render('profile', {data: req.body});
+  var data = new dbModels.StudentModel(req.body);
+  data.save()
+    .then(info => {
+      console.log("Student Info Saved to DB");
+    })
+    .catch(err => {
+      res.status(400).send("Unable to save to DB");
+    })
+  res.render('profile', { data: req.body });
 });
 
-app.get('/campusMap', function(req, res){
+app.get('/campusMap', function (req, res) {
   res.render('campusMap');
 });
+
 
 app.listen(3000);
