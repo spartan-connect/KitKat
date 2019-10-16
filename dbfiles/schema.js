f0c4b9709680560bef847f2e8bfeb5dbf225a248
Dylan Ceronio <dylanceronio@sbcglobal.net>
2019-10-15 18:05:43 -0700

    finished models and schemas for DB

--- dbfiles/schema.js
+++ dbfiles/schema.js
@@ -14,9 +14,16 @@ var StudentSchema = new mongoose.Schema({
     classList: Array,
     photo: { data: Buffer, contentType: String },
     bio: String,
-    club: Array,
-    calander: String,
-    major: String
+    club: [String],
+    calender: String,
+    major: String,
+    Messages: Array
+});
+
+var MessageSchema = new mongoose.Schema({
+    sender: String,
+    sentTo: String,
+    text: String
 });
 
 var FacultySchema = new mongoose.Schema({
@@ -36,13 +43,94 @@ var CampusPDSchema = new mongoose.Schema({
     email: String
 });
 
+var Message = mongoose.model('Message', MessageSchema);
 var CampusPD = mongoose.model('CampusPD', CampusPDSchema);
 var Faculty = mongoose.model('Faculty', FacultySchema);
 var Student = mongoose.model('Student', StudentSchema);
 
+var CalenderSchema = new mongoose.Schema({
+    calenderEvents: { type: mongoose.Schema.Types.ObjectId, ref: 'CalenderEvent' }
+});
+
+var CalenderEventSchema = new mongoose.Schema({
+    date: Date,
+    title: String,
+    description: String
+});
+
+var Calender = mongoose.model('Calender', CalenderSchema);
+var CalenderEvent = mongoose.model('CalenderEvent', CalenderEventSchema);
+
+var ClubEventSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    contact: String,
+    Photo: { data: Buffer, contentType: String }
+});
+
+var ClubEvent = mongoose.model('ClubEvent', ClubEventSchema);
+// https://stackoverflow.com/questions/18001478/referencing-another-schema-in-mongoose
+var ClubSchema = mongoose.Schema({
+    name: String,
+    description: String,
+    memberList: [String],
+    calender: { type: mongoose.Schema.Types.ObjectId, ref: 'Calender' }
+});
+
+var Club = mongoose.model('Club', ClubSchema);
+
+var FacultyAnnouncementSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    contact: String,
+    photo: { data: Buffer, contentType: String }
+});
+
+var JobPostSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    contact: String,
+    Photo: { data: Buffer, contentType: String }
+});
+
+var SafteyWarningSchema = new mongoose.Schema({
+    title: String,
+    urgency: String,
+    description: String
+});
+
+var FacultyAnnouncement = mongoose.model('FacultyAnnouncement', FacultyAnnouncementSchema);
+var JobPost = mongoose.model('JobPost', JobPostSchema);
+var SafteyWarning = mongoose.model('SafteyWarning', SafteyWarningSchema);
+
+// Saving array of Strings: https://stackoverflow.com/questions/35509611/mongoose-save-array-of-strings
+var ChannelSchema = new mongoose.Schema({
+    Name: String,
+    MemberList: [String]
+});
+
+var Channel = mongoose.model('Channel', ChannelSchema);
+
+var GrouptChatSchema = new mongoose.Schema({
+    Name: String,
+    memberList: [String]
+});
+
+var GroupChat = mongoose.model('GroupChat', GrouptChatSchema);
+
 module.exports = {
+    ClubModel: Club,
+    ChannelModel: Channel,
+    MessageModel: Message,
+    GroupChatModel: GroupChat,
     CampusPDModel: CampusPD,
     FacultyModel: Faculty,
     StudentModel: Student,
-    mongoose: mongoose
+    mongoose: mongoose,
+    CalenderModel: Calender,
+    CalenderEventModel: CalenderEvent,
+    ClubEventModel: ClubEvent,
+    FacultyAnnouncementModel: FacultyAnnouncement,
+    JobPostModel: JobPost,
+    SafteyWarningModel: SafteyWarning
 };
