2123f1cd7203cf2cc1e1e7b93966c8fa847f551b
Maan Singh <maan.vs97@gmail.com>
2019-10-31 19:04:05 -0700

    Merged Alora's branch detailedUser

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cookieSession = require('cookie-session');
 
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -26,19 +25,20 @@ require('./routes/authRoutes')(app);
 
 //for db
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 var urlencodedParser = bodyParser.urlencoded({
   extended: false
-})
+});
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
-
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.render('home');
 });
 
@@ -49,182 +49,171 @@ app.get('/', function (req, res) {
 
 // 400 status is an operation failed
 // just example of saving to db, doesn't perform login correctly
-app.post('/profile', function (req, res) {
+app.post('/profile', function(req, res) {
   var count = Object.keys(req.body).length;
   console.log(count);
   console.log(req.body);
 
   // Authorize login
   if (count == 2) {
-    dbModels.StudentModel.find(req.body, function (err, student) {
+    dbModels.StudentModel.find(req.body, function(err, student) {
       if (student.length == 0) {
         console.log("Can't Find User");
-        res.render('home', {error: 'Incorrect Username/Password'});
-      }
-
-      else if (req.body.username == student[0].username && req.body.password == student[0].password) {
+        res.render('home', { error: 'Incorrect Username/Password' });
+      } else if (
+        req.body.username == student[0].username &&
+        req.body.password == student[0].password
+      ) {
         console.log(student);
         console.log(student[0].username);
         console.log(student[0].password);
         console.log('Student Account was found');
-        res.render('profile', {data: student[0]});
+        res.render('profile', { data: student[0] });
       }
     });
   }
   // save form information into DB
   else {
     var data = new dbModels.StudentModel(req.body);
-    data.save()
+    data
+      .save()
       .then(info => {
-        console.log("Student Info Saved to DB");
+        console.log('Student Info Saved to DB');
       })
       .catch(err => {
-        res.status(400).send("Unable to save to DB");
+        res.status(400).send('Unable to save to DB');
       });
     res.render('profile', { data: req.body });
   }
 });
 
-  var data = new dbModels.StudentModel(req.body);
-  data.save()
-    .then(info => {
-      console.log("Student Info Saved to DB");
-    })
-    .catch(err => {
-      res.status(400).send("Unable to save to DB");
-    })
-  res.render('profile', {
-    data: req.body
-  });
-});
-
-app.get('/campusMap', function (req, res) {
+app.get('/campusMap', function(req, res) {
   res.render('campusMap');
 });
-app.get('/clubDirectory', function (req, res) {
+app.get('/clubDirectory', function(req, res) {
   res.render('clubDirectory');
 });
-app.get('/searchStudents', function (req, res) {
+app.get('/searchStudents', function(req, res) {
   var arrayOfStudents = [];
-  dbModels.StudentModel.find(function (err, student){
-    student.forEach(function(s){
+  dbModels.StudentModel.find(function(err, student) {
+    student.forEach(function(s) {
       arrayOfStudents.push(s.username);
     });
-    res.render('searchStudents', {data: arrayOfStudents});
+    res.render('searchStudents', { data: arrayOfStudents });
   });
 });
-app.get('/searchProfile/:name', function (req, res) {
+app.get('/searchProfile/:name', function(req, res) {
   var name = req.params.name;
-  dbModels.StudentModel.findOne({"username": name}, function (err, student){
-    if (err) return res.status(400).send("Database Error");
-    if (student) res.render('searchProfile', {data: student});
-    else res.status(400).send("Student not found");
+  dbModels.StudentModel.findOne({ username: name }, function(err, student) {
+    if (err) return res.status(400).send('Database Error');
+    if (student) res.render('searchProfile', { data: student });
+    else res.status(400).send('Student not found');
   });
 });
 
 // these are the temporary databases
-app.get('/channels', function (req, res) {
+app.get('/channels', function(req, res) {
   channelData = {
-
-    "users": [{
-        "name": 'Bob',
-        "status": 'offline'
+    users: [
+      {
+        name: 'Bob',
+        status: 'offline'
       },
       {
-        "name": 'Bob2',
-        "status": 'online'
+        name: 'Bob2',
+        status: 'online'
       },
       {
-        "name": 'Bob3',
-        "status": 'online'
+        name: 'Bob3',
+        status: 'online'
       },
       {
-        "name": 'John',
-        "status": 'offline'
+        name: 'John',
+        status: 'offline'
       },
       {
-        "name": 'Apple',
-        "status": 'offline'
+        name: 'Apple',
+        status: 'offline'
       },
       {
-        "name": 'Seed',
-        "status": 'offline'
+        name: 'Seed',
+        status: 'offline'
       },
       {
-        "name": 'Johnny',
-        "status": 'online'
+        name: 'Johnny',
+        status: 'online'
       },
       {
-        "name": 'TestName',
-        "status": 'offline'
-      }
-
-    ],
-    "messages": [{
-        "text": 'Hi everyone!'
-      },
-      {
-        "text": 'Hello everyone!'
+        name: 'TestName',
+        status: 'offline'
       }
     ],
-    
-    "channels": [{
-        "name": 'CS160',
-        "ID": 0
+    messages: [
+      {
+        text: 'Hi everyone!'
       },
       {
-        "name": 'Fun Channel',
-        "ID": 1
+        text: 'Hello everyone!'
+      }
+    ],
+
+    channels: [
+      {
+        name: 'CS160',
+        ID: 0
       },
       {
-        "name": 'Lost and Found',
-        "ID": 2
+        name: 'Fun Channel',
+        ID: 1
       },
       {
-        "name": 'Marketplace!',
-        "ID": 3
+        name: 'Lost and Found',
+        ID: 2
       },
       {
-        "name": 'AveryLongChannelNameAaaaaaaa',
-        "ID": 4
+        name: 'Marketplace!',
+        ID: 3
+      },
+      {
+        name: 'AveryLongChannelNameAaaaaaaa',
+        ID: 4
       }
     ]
-  }
+  };
 
   res.render('channels', channelData);
 });
 
-
 var MessageTest = mongoose.model('MessageTest', {
   name: String,
   message: String
-})
+});
 
 app.get('/messages', (req, res) => {
   MessageTest.find({}, (err, messages) => {
     res.send(messages);
-  })
-})
-
+  });
+});
 
 app.get('/messages/:user', (req, res) => {
-  var user = req.params.user
+  var user = req.params.user;
 
-  MessageTest.find({
-    name: user
-  }, (err, messages) => {
-    res.send(messages);
-  })
-})
-
+  MessageTest.find(
+    {
+      name: user
+    },
+    (err, messages) => {
+      res.send(messages);
+    }
+  );
+});
 
 app.post('/messages', async (req, res) => {
   try {
     var message = new MessageTest(req.body);
 
-    var savedMessage = await message.save()
+    var savedMessage = await message.save();
     console.log('saved');
-
 
     var censored = await MessageTest.findOne({
       message: 'badword'
@@ -232,23 +221,20 @@ app.post('/messages', async (req, res) => {
     if (censored)
       await MessageTest.remove({
         _id: censored.id
-      })
-    else
-      io.emit('message', req.body);
+      });
+    else io.emit('message', req.body);
     res.sendStatus(200);
   } catch (error) {
     res.sendStatus(500);
     return console.log('error', error);
   } finally {
-    console.log('Message Posted')
+    console.log('Message Posted');
   }
-
-})
-
+});
 
 io.on('connection', () => {
-  console.log('a user is connected')
-})
+  console.log('a user is connected');
+});
 
 // mongoose.connect(keys.dbURL, (err) => {
 //   console.log('mongodb connected', err);
