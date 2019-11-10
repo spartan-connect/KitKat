fd1a033322fb787e973a8ba7bca7e9ac7c2f8f24
sasaber <s.ahmsab@gmail.com>
2019-11-09 23:20:21 -0800

    remove cypress files

--- cypress/integration/test_spec.js
+++
@@ -1,7 +0,0 @@
-describe('My First Test', function() {
-    it('Visits the Kitchen Sink', function() {
-      cy.visit('http://localhost:3000/')
-      cy.contains('Login').click()
-      cy.url().should('include', '/profile')
-    })
-  })
