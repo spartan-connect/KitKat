ed724fd80848e58912713d54fe9c57d56cd25a0d
crystal9999 <44036150+crystal9999@users.noreply.github.com>
2019-11-22 15:52:08 -0800

    Update pageVisitsTests.spec.js
    Completed test case for switching between different channels on the channels page. Currently, for test case to work, go to app.js and set currentUsername to be something else other than "req.user.firstName + "" + req.user.lastName". For example:
    currentUsername = "Test Name"; //req.user.firstName + " " + req.user.lastName;

--- cypress/integration/SpartanTests/pageVisitsTests.spec.js
+++ cypress/integration/SpartanTests/pageVisitsTests.spec.js
@@ -6,9 +6,22 @@ describe('Home Page', function() {
   })
 })
 
+// Test: on channels page, switching to another channel
 describe('Channels Page', function() {
   it('Tests if channels page is working', function() {
+	cy.setCookie('currentUser', encodeURIComponent('{"currentUser":"currentUser=BobSmith;"}'));
     cy.visit('http://localhost:3000/channels');
+	cy.getCookie('viewedChannelId').should('have.property', 'value', '-1');
+	cy.get('#myChannels').click();
+	cy.getCookie('viewedChannelId').should('have.property', 'value', '3');
+	cy.contains('Fun Channel').click();
+	cy.getCookie('viewedChannelId').should('have.property', 'value', '1');
+	cy.contains('Lost and Found').click();
+	cy.getCookie('viewedChannelId').should('have.property', 'value', '2');
+	cy.contains('Marketplace!').click();
+	cy.getCookie('viewedChannelId').should('have.property', 'value', '3');
+	cy.contains('CS160').click();
+	cy.getCookie('viewedChannelId').should('have.property', 'value', '0');
   })
 })
 
