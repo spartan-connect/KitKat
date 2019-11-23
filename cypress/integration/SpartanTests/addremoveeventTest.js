15f8d6992e08e90f237f8c7951f465eb8b1f34b4
Dylan Ceronio <dylanceronio@sbcglobal.net>
2019-11-22 23:43:31 -0800

    adding remove/add event button test file

---
+++ cypress/integration/SpartanTests/addremoveeventTest.js
@@ -0,0 +1,17 @@
+describe('Add Event', function () {
+    it('Test if adding event is working', function () {
+
+    })
+});
+
+describe('Add Event, check if cannot enter "~" in Title', function () {
+    it('Test if adding an event, Test if cannot enter "~" in Title', function () {
+
+    })
+});
+
+describe('Remove Event', function () {
+    it('Test if removing an event is working', function () {
+
+    })
+});
--- cypress/integration/SpartanTests/pageVisitsTests.spec.js
+++ cypress/integration/SpartanTests/pageVisitsTests.spec.js
@@ -1,23 +1,23 @@
 
 
-describe('Home Page', function() {
-  it('Tests if home page is working', function() {
+describe('Home Page', function () {
+  it('Tests if home page is working', function () {
     cy.visit('http://localhost:3000');
   })
 })
 
-describe('Channels Page', function() {
-  it('Tests if channels page is working', function() {
+describe('Channels Page', function () {
+  it('Tests if channels page is working', function () {
     cy.visit('http://localhost:3000/channels');
   })
 })
 
-describe('Search for a Student Page', function() {
-  it('Tests if home page is working', function() {
+describe('Search for a Student Page', function () {
+  it('Tests if home page is working', function () {
     cy.visit('http://localhost:3000/searchStudents');
   })
 })
-/** 
+/**
 describe('Home Page', function() {
   it('Tests if home page is working', function() {
     cy.visit('http://localhost:3000');
