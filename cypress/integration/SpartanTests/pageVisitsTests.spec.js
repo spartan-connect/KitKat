9e44b2007cdd48648d8b9cc14a6b59b9fe8c9831
Maan Singh <maan.vs97@gmail.com>
2019-12-09 20:05:04 -0800

    added google map

--- cypress/integration/SpartanTests/pageVisitsTests.spec.js
+++ cypress/integration/SpartanTests/pageVisitsTests.spec.js
@@ -1,17 +1,17 @@
-describe('Home Page', function() {
-  it('Tests if home page is working', function() {
+describe('Home Page', function () {
+  it('Tests if home page is working', function () {
     cy.visit('http://localhost:3000');
   });
 });
 
-describe('Channels Page', function() {
-  it('Tests if channels page is working', function() {
+describe('Channels Page', function () {
+  it('Tests if channels page is working', function () {
     cy.visit('http://localhost:3000/channels');
   });
 });
 
-describe('Search for a Student Page', function() {
-  it('Tests if search for a student page is working', function() {
+describe('Search for a Student Page', function () {
+  it('Tests if search for a student page is working', function () {
     cy.visit('http://localhost:3000/searchStudents');
     cy.get('#studentList>li').each(($el, index, $list) => {
       cy.get('#studentList>li')
@@ -22,32 +22,30 @@ describe('Search for a Student Page', function() {
   });
 });
 
-describe('Profile Page', function() {
-  it('Tests if profile page is working', function() {
+describe('Profile Page', function () {
+  it('Tests if profile page is working', function () {
     cy.visit('http://localhost:3000/profile');
   });
 });
 
-describe('Campus Map Page', function() {
-  it('Tests if campus map page is working', function() {
+describe('Campus Map Page', function () {
+  it('Tests if campus map page is working', function () {
     cy.visit('http://localhost:3000/campusMap');
   });
 });
 
-describe('Club Directory Page', function() {
-  it('Tests if club directory page is working', function() {
+describe('Club Directory Page', function () {
+  it('Tests if club directory page is working', function () {
     cy.visit('http://localhost:3000/clubDirectory');
     cy.get('#clubList>li').each(($el, index, $list) => {
-      cy.get('#clubList>li')
-        .eq(index)
-        .click();
-      cy.visit('https://sjsu.my.campusapp.com/#/clubs/*****');
+      cy.get('#clubList>li').eq(0);
+      cy.visit('http://localhost:3000/clubDirectory');
     });
   });
 });
 
-describe('Event Calendar Page', function() {
-  it('Tests if event calendar page is working', function() {
+describe('Event Calendar Page', function () {
+  it('Tests if event calendar page is working', function () {
     cy.visit('http://localhost:3000/eventCalendar');
   });
 });
